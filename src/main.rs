9cc40bee743b830dcceeb76871c13f9d4f8bb5b8
matt-hu <matt.hu1@outlook.com>
2021-07-13 14:56:27 +0100

    Finish commenting main.rs

--- src/main.rs
+++ src/main.rs
@@ -74,53 +74,64 @@ async fn main() -> Result<(), IoError> {
 async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr,mut port: Arc<Mutex<serial::SystemPort>>, /*tx_serial: mpsc::Sender<String>,*/ mut cabs_threads: Arc<Mutex<Vec<Cab>>>, known_cabs_threads: Arc<Mutex<HashMap<String, u32>>>, mut track_power_threads: Arc<Mutex<TrackPower>> ) {
     println!("Incoming TCP connection from: {}", addr);
 
+    // Accept connection and report to console()
     let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
         .expect("Error during the websocket handshake occurred");
     println!("WebSocket connection established: {}", addr);
 
-    // Insert the write part of this peer to the peer map.
+    // Create peer
     let (tx, rx) = unbounded();
+    
     // These are hardcoded values, need to be changed to send the actual current value
     &tx.unbounded_send(Message::Text(r#"{"type": "config", "cabs": {"Train1": "1", "Train2", "2"}, "debug": "True"}"#.to_string())).unwrap();
     &tx.unbounded_send(Message::Text(r#"{"type": "state", "updateType": "cab", "cab": "1", "speed": 0, "direction": 1, "functions": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}"#.to_string())).unwrap();
     &tx.unbounded_send(Message::Text(r#"{"type": "state", "updateType": "cab", "cab": "2", "speed": 0, "direction": 1, "functions": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}"#.to_string())).unwrap();
+    // -------------------------------------------------------------------------------
+    
+    // Insert the write part of this peer to the peer map.
     peer_map.lock().unwrap().insert(addr, tx);
 
+
+    // Split websocket stream
     let (outgoing, incoming) = ws_stream.split();
 
 
+    // Run closure for each incoming message
     let broadcast_incoming = incoming.try_for_each(|msg| {
         println!("Received a message from {}: {}", addr, msg.to_text().unwrap());
         
-//        let tx_serial = mpsc::Sender::clone(&tx_serial);
+        // Clone a copy of the mutex handlers
         let cabs_threads = Arc::clone(&mut cabs_threads);
         let known_cabs_threads = Arc::clone(&known_cabs_threads);
         let track_power_threads = Arc::clone(&mut track_power_threads);
         let port = Arc::clone(&mut port);
 
+        // Create a blank string for the update packet
         let mut update_packet: String = String::new();
-        // println!("{}",msg);
+        // Convert the message to a string
         let msg_str = msg.to_text().unwrap().to_string();
+        // Parse packet to a PacketProt
         let packet_prot: Option<PacketProt> = match parser::parse(msg_str).unwrap() {
             Parsed::Speed(packet) => Some(update_state::speed(packet, &mut update_packet, known_cabs_threads, cabs_threads)),
             Parsed::Function(packet) => Some(update_state::function(packet, &mut update_packet, known_cabs_threads, cabs_threads)),
             Parsed::Power(packet) => Some(update_state::power(packet, &mut update_packet, track_power_threads)),
         };
 //        serial_utils::send_packet(packet_prot.unwrap(), tx_serial).unwrap();
+
+        // Obtain serial port lock
         let port = port.lock().unwrap();
+        // Write packet to serial
         if let Err(error) = serial_utils::write_packet(packet_prot.unwrap(),port) {
             println!("{}", error);
             return future::ok(());
         }
-        // return future::err(());
-        let update_msg = Message::Text(update_packet);
-        // tx_socket_controller.send(update_packet).unwrap();
-        // websocket.write_message(msg).unwrap();
 
+        // Obtain peer map lock
         let peers = peer_map.lock().unwrap();
 
-        // We want to broadcast the message to everyone except ourselves.
+        // Broadcast the message to everyone except ourselves.
+        let update_msg = Message::Text(update_packet);
         let broadcast_recipients = peers.iter().filter(|(peer_addr, _)| peer_addr != &&addr).map(|(_, ws_sink)| ws_sink);
 
         for recp in broadcast_recipients {
@@ -131,15 +142,15 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: Socke
         }
 
         future::ok(())
-        // Ok(future::ready(Ok(())))
     });
-    // let broadcast_incoming = broadcast_incoming.unwrap();
 
+    // Handle update packets
     let receive_from_others = rx.map(Ok).forward(outgoing);
 
     pin_mut!(broadcast_incoming, receive_from_others);
     future::select(broadcast_incoming, receive_from_others).await;
 
+    // Handle device disconnecting
     println!("{} disconnected", &addr);
     peer_map.lock().unwrap().remove(&addr);
 }
